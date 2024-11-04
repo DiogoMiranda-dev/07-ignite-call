79ef35a5f991e8b1ac6bd47706c0230c520be919
Diogo Miranda <diogosminfor@gmail.com>
2024-11-04 19:50:01 -0300

    finalizo o cadastro e agora falta fazer o calendario

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,19 +2,14 @@ import { globalCss } from '@ignite-ui/react'
 
 export const globalStyles = globalCss({
   '*': {
-    margin: 0,
-    padding: 0,
     boxSizing: 'border-box',
+    padding: 0,
+    margin: 0,
   },
 
   body: {
     backgroundColor: '$gray900',
     color: '$gray100',
     '-webkit-font-smoothing': 'antialiased',
-  },
-
-  'body, input, textarea, button': {
-    fontFamily: 'Roboto',
-    fontWeight: 400,
   },
 })
